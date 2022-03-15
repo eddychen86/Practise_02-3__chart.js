308fd1fff0af2b659374a981b2dd736c8d89b546
eddychen86 <eddychen101020@gmail.com>
2022-03-15 17:04:26 +0800

    Step 5: update setting.js

--- setting.js
+++ setting.js
@@ -35,6 +35,35 @@ axios
 		});
 	});
 
+// 幹線選項 API
+axios
+	.get("./trLine.json")
+	.then(function(res){
+		var datas = res.data
+		$(function(){
+			datas.map(function(data){
+				$("#trLine2").append('<option value="' + data.trLine + '">' + data.trLine + '</option>');
+			});
+		});
+	});
+
+// // 更新 data
+// function addData(chart, label, data) {
+// 	chart.data.labels.push(label);
+// 	chart.data.datasets.forEach((dataset) => {
+// 		dataset.data.push(data);
+// 	});
+// 	chart.update();
+// }
+
+// function removeData(chart) {
+// 	chart.data.labels.pop();
+// 	chart.data.datasets.forEach((dataset) => {
+// 		dataset.data.pop();
+// 	});
+// }
+// 	chart.update();
+
 // 篩選程序 API
 function getInfo1() {
 	return new Promise((resolve) => {
@@ -56,21 +85,15 @@ function getInfo1() {
 					y1: item1["gateInComingCnt"],
 					y2: item1["gateOutGoingCnt"]
 				}
-				console.log(newItem1["x"]);
+				// console.log(newItem1["x"]);
 
 				labelCount1.push(newItem1.x);
 				data1Count1.push(newItem1.y1);
 				data2Count1.push(newItem1.y2 * -1);
 			});
-			console.log(labelCount1, data1Count1, data2Count1);
+			// console.log(labelCount1, data1Count1, data2Count1);
 			
-			// (() => {
-			// 	data1.datasets[0].data = data1Count1;
-			// 	data1.datasets[1].data = data2Count1;
-			// 	data1.update();
-			// })();
-
-			var ctx1 = document.getElementById("stiNB");
+			var ctx1 = document.getElementById("myChart1");
 			const data1 = {
 				labels: labelCount1,
 				datasets: [{
@@ -122,61 +145,200 @@ function getInfo1() {
 					}
 				}
 			}
-			const stiNB = new Chart(ctx1, config1);	
+			const myChart1 = new Chart(ctx1, config1);
 		});
 	});
 }
 
 function getInfo2() {
 	return new Promise((resolve) => {
-		const date2 = $("#staDateE2").val();
-		axios.get(`https://taiwan-railway.herokuapp.com/date/${date2}`).then((response) => {
+		const trLine2 = $("#trLine2").val();
+		if ( trLine2 === "西部幹線-北" ) {
+			return 
+		} else 
+		if ( trLine2 === "西部幹線-海" ) {
+
+		} else 
+		if ( trLine2 === "西部幹線-山" ) {
+
+		} else 
+		if ( trLine2 === "西部幹線-彰雲嘉" ) {
+
+		} else 
+		if ( trLine2 === "西部幹線-台南高屏" ) {
+
+		} else 
+		if ( trLine2 === "南迴線" ) {
+
+		} else 
+		if ( trLine2 === "東部幹線-台東" ) {
+
+		} else 
+		if ( trLine2 === "東部幹線-花蓮" ) {
+
+		} else 
+		if ( trLine2 === "東部幹線-宜蘭&宜蘭縣" ) {
+
+		} else 
+		if ( trLine2 === "其他" ) {
+
+		}
+		const dateS2 = $("#staDateS2").val();
+		const dateE2 = $("#staDateE2").val();
+		axios.get(`https://taiwan-railway.herokuapp.com/八堵/920/${dateS2}/${dateE2}`).then((response) => {
 			resolve(response.data);
 			let resp2 = response.data;
-			// console.log(resp2);
+			// console.log(resp);
 
 			let labelCount2 = [];
+			let data1Count2 = [];
+			let data2Count2 = [];
+
+			resp2.map((item2) => {
+				let newItem2 = { 
+					x: item2["trnOpDate"],
+					y1: item2["gateInComingCnt"],
+					y2: item2["gateOutGoingCnt"]
+				}
+				// console.log(newItem1["x"]);
+
+				labelCount2.push(newItem2.x);
+				data1Count2.push(newItem2.y1);
+				data2Count2.push(newItem2.y2);
+			});
+			// console.log(labelCount1, data1Count1, data2Count1);
+			
+			var ctx2 = document.getElementById("myChart2");
+			const data2 = {
+				labels: labelCount2,
+				datasets: [{
+					label: "進站人數",
+					data: data1Count2,
+					fill: true,
+					backgroundColor: "rgba(98, 54, 245, 0.2)",
+					borderColor: "rgba(98, 54, 245)",
+					tension: 0.1
+				},{
+					label: "出站人數",
+					data: data2Count2,
+					fill: true,
+					backgroundColor: "rgba(255, 89, 89, 0.2)",
+					borderColor: "rgba(255, 89, 89)",
+					tension: 0.1
+				}]
+			}
+			const config2 = {
+				type: "polarArea",
+				data: data3
+			}
+			const myChart2 = new Chart(ctx2, config2);
+		});
+	});
+}
+
+function getInfo3() {
+	return new Promise((resolve) => {
+		const sta3 = $("#staName3").val();
+		const dateS3 = $("#staDateS3").val();
+		axios.get(`https://taiwan-railway.herokuapp.com/${sta3}/${dateS3}`).then((response) => {
+			resolve(response.data);
+			let resp3 = response.data;
+
+			let labelCount3 = [];
+			let data1Count3 = [];			
+
+			resp3.map((item3) => {
+				let newItem3 = { 
+					x: item3["trnOpDate"],
+					y1: item3["gateInComingCnt"],
+					y2: item3["gateOutGoingCnt"]
+				}
+
+				labelCount3.push(newItem3.x);
+				data1Count3.push(newItem3.y1, newItem3.y2);
+			});
+			console.log(data1Count3);
+			
+			var ctx3 = document.getElementById("myChart3");
+			const data3 = {
+				labels: ["進站人數", "出站人數"],
+				datasets: [{
+					label: "進站人數",
+					data: data1Count3,
+					backgroundColor: ["rgba(98, 54, 245)", "rgba(255, 89, 89)"],
+				}]
+			}
+			const config3 = {
+				type: "pie",
+				data: data3,
+				options: {
+					aspectRatio: 1.5,
+					plugins: {
+						legend: {
+							position: "right"
+						}
+					}
+				}
+			}
+			const myChart3 = new Chart(ctx3, config3);
+		});
+	});
+}
+
+function getInfo4() {
+	return new Promise((resolve) => {
+		const date4 = $("#staDateE4").val();
+		axios.get(`https://taiwan-railway.herokuapp.com/date/${date4}`).then((response) => {
+			resolve(response.data);
+			let resp4 = response.data;
+			// console.log(resp2);
+
+			let labelCount4 = [];
 
 			let total1 = [];	let total2 = [];	let total3 = [];	let total4 = [];	let total5 = [];
-			let total6 = [];	let total7 = []; let total8 = [];	let total9 = [];
+			let total6 = [];	let total7 = []; let total8 = [];	let total9 = []; let total10 = [];
 
-			resp2.map((item) => {
-				let newItem2 = { 
+			resp4.map((item) => {
+				let newItem4 = { 
 					x: item["staCode"],
 					y: item["gateInComingCnt"] + item["gateOutGoingCnt"]
 				}
-
-				if ( newItem2.x >= 900 && newItem2.x <= 1250 ) {
-					total1.push(newItem2.y);
-					labelCount2.push("西部幹線-北");
-				} else if ( newItem2.x >= 2110 && newItem2.x <= 2260 ) {
-					total2.push(newItem2.y)
-					labelCount2.push("西部幹線-海");
-				} else if ( newItem2.x >= 3140 && newItem2.x <= 3350 ) {
-					total3.push(newItem2.y)
-					labelCount2.push("西部幹線-山");
-				} else if ( newItem2.x >= 3360 && newItem2.x <= 4100 ) {
-					total4.push(newItem2.y)
-					labelCount2.push("西部幹線-彰雲嘉");
-				} else if ( newItem2.x >= 4110 && newItem2.x <= 5110 ) {
-					total5.push(newItem2.y)
-					labelCount2.push("西部幹線-台南高屏");
-				} else if ( newItem2.x >= 5120 && newItem2.x <= 5240 ) {
-					total6.push(newItem2.y)
-					labelCount2.push("南迴線");
-				} else if ( newItem2.x >= 6000 && newItem2.x <= 6070 ) {
-					total7.push(newItem2.y)
-					labelCount2.push("東部幹線-台東");
-				} else if ( newItem2.x >= 6080 && newItem2.x <= 7080 ) {
-					total8.push(newItem2.y)
-					labelCount2.push("東部幹線-花蓮");
-				} else if ( newItem2.x >= 7090 && newItem2.x <= 7360 ) {
-					total9.push(newItem2.y)
-					labelCount2.push("其他");
+				
+				// 參考：https://www.fly.idv.tw/
+				if ( newItem4.x >= 900 && newItem4.x <= 1250 ) {
+					total1.push(newItem4.y);
+					labelCount4.push("西部幹線-北");
+				} else if ( newItem4.x >= 2110 && newItem4.x <= 2260 ) {
+					total2.push(newItem4.y)
+					labelCount4.push("西部幹線-海");
+				} else if ( newItem4.x >= 3140 && newItem4.x <= 3350 ) {
+					total3.push(newItem4.y)
+					labelCount4.push("西部幹線-山");
+				} else if ( newItem4.x >= 3360 && newItem4.x <= 4100 ) {
+					total4.push(newItem4.y)
+					labelCount4.push("西部幹線-彰雲嘉");
+				} else if ( newItem4.x >= 4110 && newItem4.x <= 5110 ) {
+					total5.push(newItem4.y)
+					labelCount4.push("西部幹線-台南高屏");
+				} else if ( newItem4.x >= 5120 && newItem4.x <= 5240 ) {
+					total6.push(newItem4.y)
+					labelCount4.push("南迴線");
+				} else if ( newItem4.x >= 6000 && newItem4.x <= 6070 ) {
+					total7.push(newItem4.y)
+					labelCount4.push("東部幹線-台東");
+				} else if ( newItem4.x >= 6080 && newItem4.x <= 7080 ) {
+					total8.push(newItem4.y)
+					labelCount4.push("東部幹線-花蓮");
+				} else if ( newItem4.x >= 7090 && newItem4.x <= 7360 ) {
+					total9.push(newItem4.y)
+					labelCount4.push("東部幹線-宜蘭&宜蘭縣");
+				} else if ( newItem4.x >= 7361 && newItem4.x <= 7390 ) {
+					total10.push(newItem4.y)
+					labelCount4.push("其他");
 				}
 			});
 
-			let data1Count2 = [];
+			let data1Count4 = [];
 
 			function sumData(arr) {
 				let sum = 0;
@@ -186,18 +348,19 @@ function getInfo2() {
 				return sum;
 			}
 
-			data1Count2.push(
+			data1Count4.push(
 				sumData(total1), sumData(total2), sumData(total3),
 				sumData(total4), sumData(total5), sumData(total6),
-				sumData(total7), sumData(total8), sumData(total9)
+				sumData(total7), sumData(total8), sumData(total9),
+				sumData(total10)
 			);
 
-			console.log(data1Count2);
+			// console.log(data1Count2);
 
-			const labelCount2_2 = [...new Set(labelCount2)];
+			const labelCount4_2 = [...new Set(labelCount4)];
 			// console.log(labelCount2_2);
 			
-			var ctx2 = document.getElementById("cityNB");
+			var ctx4 = document.getElementById("myChart4");
 
 			// function getRandomColor() {
 			// 	let letters = '0123456789ABCDEF'.split('');
@@ -211,28 +374,35 @@ function getInfo2() {
 			// 	colors.push(getRandomColor());
 			// }
 			
-			const data2 = {
-				labels: labelCount2_2,
+			const data4 = {
+				labels: labelCount4_2,
 				datasets: [{
-					data: data1Count2,
+					data: data1Count4,
 					backgroundColor: 
 					[
-						"rgba(184, 216, 216, 1)", "rgba(122, 158, 159, 1)", "rgba(79, 99, 103, 1)",
-						"rgba(159, 172, 161, 1)", "rgba(199, 209, 190, 1)", "rgba(238, 245, 219, 1)",
-						"rgba(246, 170, 152, 1)", "rgba(250, 133, 119, 1)", "rgba(254, 95, 85, 1)"
+						"rgba(184, 216, 216)", "rgba(122, 158, 159)", "rgba(79, 99, 103)",
+						"rgba(159, 172, 161)", "rgba(199, 209, 190)", "rgba(238, 245, 219)",
+						"rgba(246, 170, 152)", "rgba(250, 133, 119)", "rgba(254, 95, 85)",
+						"rgba(204, 232, 204)"
 					],
 					hoverOffset: 4
 				}],
 			}
-			const config2 = {
+			const config4 = {
 				type: "doughnut",
-				data: data2,
+				data: data4,
 				options: {
 					// 搭配 隨機變色 colors 
 					// hover: {mode: null},
+					aspectRatio: 1.5,
+					plugins: {
+						legend: {
+							position: "right"
+						}
+					}
 				}
 			}
-			const cityNB = new Chart(ctx2, config2);		
+			const myChart4 = new Chart(ctx4, config4);		
 		});
 	});
 }
