b5b8d88554bcb38f67543cb911d65a906c86dad5
eddychen86 <eddychen101020@gmail.com>
2022-03-17 09:14:59 +0800

    Step 5: update setting.js

--- setting.js
+++ setting.js
@@ -145,33 +145,108 @@ function getInfo1() {
 
 function getInfo2() {
 	return new Promise((resolve) => {
-		const trLine = $("#trLine2").val();
-		console.log(trLine);
+		const trLine2 = $("#trLine2").val();
 		const dateS2 = $("#staDateS2").val();
 		const dateE2 = $("#staDateE2").val();
-		axios.get(`https://taiwan-railway.herokuapp.com/line/${trLine}/${dateS2}/${dateE2}`).then((response) => {
+
+		axios.get(`https://taiwan-railway.herokuapp.com/八堵/910/${dateS2}/${dateE2}`).then((response) => {
 			resolve(response.data);
 			const resp2 = response.data;
-			console.log(resp2);
 
-			const labelCount2 = [];
-			const data1Count2 = [];
-			const data2Count2 = [];
+			// const date = [];
+
+			const west_north_in = []; const west_north_out = []; const west_north_all = [];
+			const west_ocean_in = []; const west_ocean_out = []; const west_ocean_all = [];
+			const west_mount_in = []; const west_mount_out = []; const west_mount_all = [];
+			const west_cyc_in = []; const west_cyc_out = []; const west_cyc_all = [];
+			const west_tk_in = []; const west_tk_out = []; const west_tk_all = [];
+			const south_in = []; const south_out = []; const south_all = [];
+			const east_taitung_in = []; const east_taitung_out = []; const east_taitung_all = [];
+			const east_hualien_in = []; const east_hualien_out = []; const east_hualien_all = [];
+			const east_yilan_in = []; const east_yilan_out = []; const east_yilan_all = [];
+			const other_in = []; const other_out = []; const other_all = [];
 
 			resp2.map((item2) => {
 				let newItem2 = { 
-					x: item2["trnOpDate"],
+					x: item2["staCode"],
 					y1: item2["gateInComingCnt"],
 					y2: item2["gateOutGoingCnt"]
 				}
-				// console.log(newItem1["x"]);
+				// console.log(newItem2.x);
 
-				labelCount2.push(newItem2.x);
-				data1Count2.push(newItem2.y1);
-				data2Count2.push(newItem2.y2);
+				if ( newItem2.x >= 900 && newItem2.x <= 1250 ) {
+					west_north_in.push(newItem2.y1); west_north_out.push(newItem2.y2);
+				} else if ( newItem2.x >= 2110 && newItem2.x <= 2260 ) {
+					west_ocean_in.push(newItem2.y1); west_ocean_out.push(newItem2.y2);
+				} else if ( newItem2.x >= 3140 && newItem2.x <= 3350 ) {
+					west_mount_in.push(newItem2.y1); west_mount_out.push(newItem2.y2);
+				} else if ( newItem2.x >= 3360 && newItem2.x <= 4100 ) {
+					west_cyc_in.push(newItem2.y1); west_cyc_out.push(newItem2.y2);
+				} else if ( newItem2.x >= 4110 && newItem2.x <= 5110 ) {
+					west_tk_in.push(newItem2.y1); west_tk_out.push(newItem2.y2);
+				} else if ( newItem2.x >= 5120 && newItem2.x <= 5240 ) {
+					south_in.push(newItem2.y1); south_out.push(newItem2.y2);
+				} else if ( newItem2.x >= 6000 && newItem2.x <= 6070 ) {
+					east_taitung_in.push(newItem2.y1); east_taitung_out.push(newItem2.y2);
+				} else if ( newItem2.x >= 6080 && newItem2.x <= 7080 ) {
+					east_hualien_in.push(newItem2.y1); east_hualien_out.push(newItem2.y2);
+				} else if ( newItem2.x >= 7090 && newItem2.x <= 7360 ) {
+					east_yilan_in.push(newItem2.y1); east_yilan_out.push(newItem2.y2);
+				} else if ( newItem2.x >= 7361 && newItem2.x <= 7390 ) {
+					other_in.push(newItem2.y1); other_out.push(newItem2.y2);
+				}
 			});
-			// console.log(labelCount1, data1Count1, data2Count1);
 			
+
+			function sumData(arr) {
+				let sum = 0;
+				arr.forEach((e) => sum += e);
+				return sum;
+			}
+
+			west_north_all.push("西部幹線-北", sumData(west_north_in), sumData(west_north_out));
+			west_ocean_all.push("西部幹線-海", sumData(west_ocean_in), sumData(west_ocean_out));
+			west_mount_all.push("西部幹線-山", sumData(west_mount_in), sumData(west_mount_out));
+			west_cyc_all.push("西部幹線-彰雲嘉", sumData(west_cyc_in), sumData(west_cyc_out));
+			west_tk_all.push("西部幹線-台南高屏", sumData(west_tk_in), sumData(west_tk_out));
+			south_all.push("南迴線", sumData(south_in), sumData(south_out));
+			east_taitung_all.push("東部幹線-台東", sumData(east_taitung_in), sumData(east_taitung_out));
+			east_hualien_all.push("東部幹線-花蓮", sumData(east_hualien_in), sumData(east_hualien_out));
+			east_yilan_all.push("東部幹線-宜蘭&宜蘭縣", sumData(east_yilan_in), sumData(east_yilan_out));
+			other_all.push("其他", sumData(other_in), sumData(other_out));
+
+			if ( trLine2 === "west-north" ) {
+				console.log(west_north_all);
+				return west_north_all
+			} else if ( trLine2 === "west-ocean" ) {
+				console.log(west_ocean_all);
+				return west_ocean_all
+			} else if ( trLine2 === "west-mount" ) {
+				console.log(west_mount_all);
+				return west_mount_all
+			} else if ( trLine2 === "west-cyc" ) {
+				console.log(west_cyc_all);
+				return west_cyc_all
+			} else if ( trLine2 === "west-tk" ) {
+				console.log(west_tk_all);
+				return west_tk_all
+			} else if ( trLine2 === "south" ) {
+				console.log(south_all);
+				return south_all
+			} else if ( trLine2 === "east-taitung" ) {
+				console.log(east_taitung_all);
+				return east_taitung_all
+			} else if ( trLine2 === "east-hualien" ) {
+				console.log(east_hualien_all);
+				return east_hualien_all
+			} else if ( trLine2 === "east-yilan" ) {
+				console.log(east_yilan_all);
+				return east_yilan_all
+			} else if ( trLine2 === "other") {
+				console.log(other_all);
+				return other_all
+			}
+
 			const ctx2 = document.getElementById("myChart2");
 			const data2 = {
 				labels: labelCount2,
@@ -191,6 +266,55 @@ function getInfo2() {
 		});
 	});
 }
+
+// function getInfo2() {
+// 	return new Promise((resolve) => {
+// 		const trLine = $("#trLine2").val();
+// 		console.log(trLine);
+// 		const dateS2 = $("#staDateS2").val();
+// 		const dateE2 = $("#staDateE2").val();
+// 		axios.get(`https://taiwan-railway.herokuapp.com/line/${trLine}/${dateS2}/${dateE2}`).then((response) => {
+// 			resolve(response.data);
+// 			const resp2 = response.data;
+// 			console.log(resp2);
+
+// 			const labelCount2 = [];
+// 			const data1Count2 = [];
+// 			const data2Count2 = [];
+
+// 			resp2.map((item2) => {
+// 				let newItem2 = { 
+// 					x: item2["trnOpDate"],
+// 					y1: item2["gateInComingCnt"],
+// 					y2: item2["gateOutGoingCnt"]
+// 				}
+// 				// console.log(newItem1["x"]);
+
+// 				labelCount2.push(newItem2.x);
+// 				data1Count2.push(newItem2.y1);
+// 				data2Count2.push(newItem2.y2);
+// 			});
+// 			// console.log(labelCount1, data1Count1, data2Count1);
+			
+			// const ctx2 = document.getElementById("myChart2");
+			// const data2 = {
+			// 	labels: labelCount2,
+			// 	datasets: [{
+			// 		data: data1Count2,
+			// 		fill: true,
+			// 		backgroundColor: "rgba(98, 54, 245, 0.2)",
+			// 		borderColor: "rgba(98, 54, 245)",
+			// 		tension: 0.1
+			// 	}]
+			// }
+			// const config2 = {
+			// 	type: "line",
+			// 	data: data2
+			// }
+			// const myChart2 = new Chart(ctx2, config2);
+// 		});
+// 	});
+// }
 
 function getInfo3() {
 	return new Promise((resolve) => {
